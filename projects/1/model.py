9b837babcb4cebd5a7a857d997e409ea0100f340
thekirillisupov <33103725+thekirillisupov@users.noreply.github.com>
2022-02-26 09:59:25 +0300

    Update model.py

--- projects/1/model.py
+++ projects/1/model.py
@@ -8,6 +8,8 @@ from sklearn.model_selection import train_test_split, GridSearchCV
 #
 # Dataset fields
 #
+
+# We create the preprocessing pipelines for both numeric and categorical data.
 numeric_features = ["if"+str(i) for i in range(1,14)]
 categorical_features = ["cf"+str(i) for i in range(1,27)] + ["day_number"]
 
@@ -15,18 +17,14 @@ fields = ["id", "label"] + numeric_features + categorical_features
 #
 # Model pipeline
 #
-
-# We create the preprocessing pipelines for both numeric and categorical data.
-numeric_features = ['CLEANLINESS', 'ROOM', 'SERVICE', 'LOCATION']
 numeric_transformer = Pipeline(steps=[
     ('imputer', SimpleImputer(strategy='median')),
 #    ('scaler', StandardScaler())
 ])
 
-categorical_features = ['city', 'country']
 categorical_transformer = Pipeline(steps=[
     ('imputer', SimpleImputer(strategy='constant', fill_value='missing')),
-    ('onehot', OneHotEncoder(handle_unknown='ignore'))
+    ('labelencoder', OrdinalEncoder())
 ])
 
 preprocessor = ColumnTransformer(
@@ -36,9 +34,8 @@ preprocessor = ColumnTransformer(
     ]
 )
 
-# Now we have a full prediction pipeline.
 model = Pipeline(steps=[
     ('preprocessor', preprocessor),
-    ('linearregression', LinearRegression())
+    ('xgboost', XGBClassifier(use_label_encoder=False))
 ])
 

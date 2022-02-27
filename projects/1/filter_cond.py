0710f79b529bc139792a2d6a1c724819de9bae0f
Kirill <wonderful.kira@gmail.com>
2022-02-27 13:41:19 +0000

    fuck

--- projects/1/filter_cond.py
+++ projects/1/filter_cond.py
@@ -6,7 +6,7 @@ def filter_cond(line_dict):
     Returns True if conditions are satisfied
     """
     cond_match = (
-       int(line_dict["num_reviews"]) > 20
+       20 < int(line_dict["if1"]) < 40 
     ) 
     return True if cond_match else False
 

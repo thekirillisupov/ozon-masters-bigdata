3c4612af2336c66e184b5f161356b85c42020305
Kirill <wonderful.kira@gmail.com>
2022-02-28 07:25:27 +0000

    add log

--- projects/1/filter_cond.py
+++ projects/1/filter_cond.py
@@ -1,10 +1,13 @@
 #
 #
+import logging
+
 def filter_cond(line_dict):
     """Filter function
     Takes a dict with field names as argument
     Returns True if conditions are satisfied
     """
+    logging.info("LINE-DICT 0 elem {}".format(list(line_dict.keys())[0])
     cond_match = (
        20 < int(line_dict["if1"]) < 40 
     ) 

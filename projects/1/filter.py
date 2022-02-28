c0c976463efa9d3fdc6707bed45dbd62efa99bc3
Kirill <wonderful.kira@gmail.com>
2022-02-28 12:09:27 +0000

    files

--- projects/1/filter.py
+++ projects/1/filter.py
@@ -68,8 +68,8 @@ for line in sys.stdin:
     #unpack into a tuple/dict
     values = line.rstrip().split('\t')
     hotel_record = dict(zip(fields, values)) #Hotel(values1)
-    #logging.info("dict {}".format(hotel_record))
-    #logging.info("outfields {}".format(outfields))
+    logging.info("dict {}".format(hotel_record))
+    logging.info("outfields {}".format(outfields))
     #apply filter conditions
     if filter_cond(hotel_record):
         output = ",".join([hotel_record[x] for x in outfields])

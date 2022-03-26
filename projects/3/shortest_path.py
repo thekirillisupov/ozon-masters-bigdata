f7ec7b45567ddffd9955a12a702f3a22906b1c71
Kirill <wonderful.kira@gmail.com>
2022-03-26 14:09:18 +0000

    go

--- projects/3/shortest_path.py
+++ projects/3/shortest_path.py
@@ -16,6 +16,14 @@ df = spark.read.csv(str(sys.argv[3]),sep="\t").cache()
 node1 = int(sys.argv[1])
 node2 = int(sys.argv[2])
 
+n1 = df.filter(df._c0 == 34)
+
+n2 = df.filter(df._c1 == 12)
+
+n1.createOrReplaceTempView("n1_table")
+
+n2.createOrReplaceTempView("n2_table")
+
 def shortest_path(node1, node2):
     path_list = [[node1]]
     path_index = 0
@@ -52,14 +60,45 @@ for i in range(len(res)):
     resStr += str(res[i])+','
 resStr[:-1]
 
-#file1 = open(str(sys.argv[4])+".csv", "w")  # write mode
-#file1.write(resStr[:-1])
-#file1.close()
+query = """
+SELECT n1_table._c0, n1_table._c1, n2_table._c1
+FROM
+n1_table JOIN n2_table
+ON n1_table._c1 = n2_table._c0
+"""
+
+res = spark.sql(query)
+
+#resRow
+
+resRow = res.collect()
+
+data = []
+for i in range(len(resRow)):
+    resRowstr = ''
+    for j in range(3):
+        resRowstr += resRow[i][2-j] + ','
+    data.append(resRowstr[:-1])
+
+#data
 
 from pyspark.sql import Row
 
 resdf = spark.createDataFrame([
-    Row(resStr[:-1])
+    Row(data[0]),
+    Row(data[1]),
+    Row(data[2]),
+    Row(data[3]),
+    Row(data[4])
 ])
+#resdf
+
+#file1 = open(str(sys.argv[4])+".csv", "w")  # write mode
+#file1.write(resStr[:-1])
+#file1.close()
+
+#resdf = spark.createDataFrame([
+#    Row(resStr[:-1])
+#])
 
 resdf.write.csv(str(sys.argv[4]))

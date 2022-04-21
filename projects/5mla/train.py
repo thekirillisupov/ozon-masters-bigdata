8d6090a68e41d9dfa349e226eb6b344d96859c5b
thekirillisupov <33103725+thekirillisupov@users.noreply.github.com>
2022-04-21 12:59:48 -0700

    Update train.py

--- projects/5mla/train.py
+++ projects/5mla/train.py
@@ -1,73 +1,79 @@
 #!/opt/conda/envs/dsenv/bin/python
 import mlflow
 import os, sys
-#import logging
-
 import pandas as pd
 from sklearn.model_selection import train_test_split
-#from joblib import dump
 
-#
-# Import model definition
-#
-from model import model, fields
-
-
-#
-# Logging initialization
-#
-#logging.basicConfig(level=logging.DEBUG)
-#logging.info("CURRENT_DIR {}".format(os.getcwd()))
-#logging.info("SCRIPT CALLED AS {}".format(sys.argv[0]))
-#logging.info("ARGS {}".format(sys.argv[1:]))
-
-#
-# Read script arguments
-#
-try:
-  #proj_id = sys.argv[1] 
-  train_path = sys.argv[1]
-  param = sys.argv[2]
-#except:
-  #logging.critical("Need to pass both project_id and train dataset path")
-  #sys.exit(1)
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import StandardScaler, OneHotEncoder, OrdinalEncoder, Normalizer
+from sklearn.linear_model import LinearRegression, LogisticRegression
+from sklearn.model_selection import train_test_split, GridSearchCV
 
 
-#logging.info(f"TRAIN_ID {proj_id}")
-#logging.info(f"TRAIN_PATH {train_path}")
 
-#
-# Read dataset
-#
+numeric_features = ["if"+str(i) for i in range(1,14)]
+categorical_features = ["cf"+str(i) for i in range(1,27)] + ["day_number"]
+fields = ["id", "label"] + numeric_features + categorical_features
+fieldswithoutlabel = ["id"] + numeric_features + categorical_features
 
-read_table_opts = dict(sep="\t", names=fields, index_col=False)
-df = pd.read_table(train_path, **read_table_opts)
-#choose short cat features
-arr_num = [i for i in range(15)]
-arr_cat = [20,23,28,31,34,36,37]
-arr_fin=arr_cat+arr_num
-#df = df.iloc[:,sorted(arr_fin)]
-#print(df.columns)
-#split train/test
-X_train, X_test, y_train, y_test = train_test_split(
-    df.iloc[:,2:], df.iloc[:,1], test_size=0.33, random_state=41
+def select(X):
+    ans=[]
+    s={"cf"+str(i) for i in range(1,27)}
+    s.add("day_number")
+    for x in X:
+        if x in s:
+            if len(X[x].unique()) < 20:
+                ans.append(x)
+    return ans
+
+# We create the preprocessing pipelines for both numeric and categorical data.
+numeric_transformer = Pipeline(steps=[
+    ('imputer', SimpleImputer(strategy='median')),
+    ('scaler', Normalizer())
+])
+
+categorical_transformer = Pipeline(steps=[
+    ('imputer', SimpleImputer(strategy='constant', fill_value='missing')),
+	('onehot', OneHotEncoder(handle_unknown='ignore'))
+])
+
+preprocessor = ColumnTransformer(
+    transformers=[
+        ('num', numeric_transformer, numeric_features),
+        ('cat', categorical_transformer, select)
+    ],
 )
 
-#
-# Train the model
-#
+# Now we have a full prediction pipeline.
+model = Pipeline(steps=[
+    ('preprocessor', preprocessor),
+    ('linearregression', LogisticRegression(max_iter=100, solver='sag',penalty='l2',C=0.9))
+])
+
+
+if __name__ == "__main__":
+    train_path = sys.argv[1]
+    paramC = sys.argv[2]
+
+    read_table_opts = dict(sep="\t", names=fields, index_col=False)
+    df = pd.read_table(train_path, **read_table_opts)
+    arr_num = [i for i in range(15)]
+    arr_cat = [20,23,28,31,34,36,37]
+    arr_fin=arr_cat+arr_num
+    X_train, X_test, y_train, y_test = train_test_split(
+        df.iloc[:,2:], df.iloc[:,1], test_size=0.33, random_state=41
+    )
 
 
 
-with mlflow.start_run():
-    model.fit(X_train, y_train)
-    model_score = model.score(X_test, y_test)
-    mlflow.log_metric("ROC AUC", model_score)
-    mlflow.sklearn.log_model(model, "model")
 
-#logging.info(f"model score: {model_score:.3f}")
+    with mlflow.start_run():
+        model.fit(X_train, y_train)
+        model_score = model.score(X_test, y_test)
+        mlflow.log_metric("ROC AUC", model_score)
+        mlflow.sklearn.log_model(model, "model")
 
-# save the model
-#dump(model, "{}.joblib".format(proj_id))
 
 

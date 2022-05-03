8cb76506b3420226b9960aff0c4a547b008a13e6
thekirillisupov <33103725+thekirillisupov@users.noreply.github.com>
2022-05-03 02:01:23 -0700

    Update train.py

--- projects/6/airflow/train.py
+++ projects/6/airflow/train.py
@@ -1,28 +1,17 @@
-from pyspark.ml.feature import *
-from sklearn.metrics import classification_report, precision_score
-from pyspark.sql import functions as f
-from pyspark.sql.types import *
-from pyspark.ml.classification import LogisticRegression
-from pyspark.ml import Pipeline
-from pyspark.ml.pipeline import Transformer
+from sklearn.linear_model import LogisticRegression
+import sys
+from sklearn.extarnals import joblib
 
 
-tokenizerBig = Tokenizer(inputCol="reviewText", outputCol="wordsBig")
-stop_words = StopWordsRemover.loadDefaultStopWords("english")
-swr = StopWordsRemover(inputCol=tokenizerBig.getOutputCol(), outputCol="words_filtered", stopWords=stop_words)
-count_vectorizerBig = CountVectorizer(inputCol=swr.getOutputCol(), outputCol="features")
-lr = LogisticRegression(featuresCol="features", labelCol="overall", maxIter=10, regParam=0)
 
-pipeline = Pipeline(stages=[
-    tokenizerBig,
-    swr,
-    count_vectorizerBig,
-    lr
-])
+path_train = sys.argv[1]
+path_model = sys.argv[2]
 
-model_path = sys.argv[1]
-train_path = sys.argv[2]
+df= pd.read_json(path_train,lines=True)
+df_train = pd.DataFrame(df.features.tolist(), index= df.index)
+df_train['label'] = df['label']
+model = LogisticRegression(max_iter=100)
+model.fit(df_train.drop(['label'], axis=1), df_train['label'])
 
-df_train = spark.read.json(train_path)
-pipeline_model = pipeline_model.fit(df_train)
-pipeline_model.write().overwrite().save(model_path)
+joblib.dump(model, path_model)
+                                                     

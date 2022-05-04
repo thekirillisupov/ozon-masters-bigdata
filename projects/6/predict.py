52d35627c7fa4b9443d88c65ff7383ee104de04d
thekirillisupov <33103725+thekirillisupov@users.noreply.github.com>
2022-05-04 01:05:11 -0700

    Update predict.py

--- projects/6/predict.py
+++ projects/6/predict.py
@@ -1,3 +1,12 @@
+
+from pyspark import SparkConf
+from pyspark.sql import SparkSession
+import pyspark.sql.functions as f
+from pyspark.ml.functions import vector_to_array
+
+from pyspark import SparkConf
+from pyspark.sql import SparkSession
+
 from pyspark.sql.types import *
 from pyspark.ml.linalg import VectorUDT
 import joblib
@@ -5,11 +14,15 @@ import pyspark.sql.functions as F
 import pandas as pd
 from pyspark.ml.linalg import DenseVector
 
-path_test = './thekirillisupov_test_out'
-path_model = './6.joblib'
-path_predict = './thekirillisupov_hw6_prediction'
+spark = SparkSession.builder.getOrCreate()
+spark.sparkContext.setLogLevel('WARN')
+
+path_test = 'thekirillisupov_test_out'
+path_model = '/home/ubuntu/ozonmasters-checkers/hws/6/thekirillisupov/8/ozon-masters-bigdata/projects/6/6.joblib'
+path_predict = 'thekirillisupov_hw6_prediction'
 
 schema = StructType(fields=[
+    StructField("id", StringType()),
     StructField("features", ArrayType(FloatType(),False))
 ])
 
@@ -25,5 +38,5 @@ def predict(series):
     predictions = est_broadcast.value.predict(series.tolist()) # Don't forget to use tolist() method
     return pd.Series(predictions)
 
-df_test = df_test.withColumn("prediction", predict("features")).select('prediction')
-df_test.repartition(1).write.format("csv").mode("overwrite").save(path_predict)
+df_test = df_test.select("id", "features").withColumn("prediction", predict("features"))
+df_test.select("id", "prediction").repartition(1).write.mode("overwrite").csv(path_predict)
